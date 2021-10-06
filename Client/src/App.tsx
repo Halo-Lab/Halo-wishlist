1473bea888b5c39cdc5e702e07ede6546e3f8c18
GephestUA <gephestua@gmail.com>
2021-10-06 13:53:27 +0300

    added simple login form

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,44 +1,9 @@
 import React, { FC, useState } from 'react';
 
-async function postData(url = '', data = {}) {
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  return await response.json();
-}
+import LoginForm from './components/LoginFort';
 
 const App: FC = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-
-  return (
-    <div>
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        type="text"
-        placeholder="Email"
-      />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        type="password"
-        placeholder="Пароль"
-      />
-      <button
-        onClick={() =>
-          postData('http://localhost:5000/api/login', { email, password })
-        }
-      >
-        Login
-      </button>
-      <button onClick={() => null}>Logout</button>
-    </div>
-  );
+  return <LoginForm />;
 };
 
 export default App;
---
+++ Client/src/components/LoginFort.tsx
@@ -0,0 +1,35 @@
+import React, { FC, useState } from 'react';
+
+import AuthRequest from '../api/request/AuthRequest';
+
+const LoginForm: FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        type="password"
+        placeholder="Пароль"
+      />
+      <button
+        onClick={async () => {
+          AuthRequest.login(email, password);
+        }}
+      >
+        Login
+      </button>
+      <button onClick={() => null}>Logout</button>
+    </div>
+  );
+};
+
+export default LoginForm;
