cd13a2858e100403b939d539cb698c0cf59694fe
GephestUA <gephestua@gmail.com>
2021-11-02 18:24:45 +0200

    UI Profile some style

--- Client/src/scenes/ProfilePage/components/MainProfile.tsx
+++ Client/src/scenes/ProfilePage/components/MainProfile.tsx
@@ -5,6 +5,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { FC } from 'react';
 
+import { DateInput } from '../../../components/common/DateInput/DateInput';
 import Icon from '../../../components/common/IconComponent/Icon';
 import Image from '../../../components/common/ImageComponent/Image';
 
@@ -24,11 +25,31 @@ export const MainProfile: FC = () => {
           alt="User Photo"
           className={styles.userPhoto}
         />
-        <h2>Tom Cruise</h2>
-        <div>
-          <Icon name={faFacebook} size="lg" />
-          <Icon name={faInstagram} size="lg" />
-          <Icon name={faTwitter} size="lg" />
+        <div className={styles.info}>
+          <h2>Tom Cruise</h2>
+          <div className={styles.icons}>
+            <Icon name={faFacebook} size="lg" />
+            <Icon name={faInstagram} size="lg" />
+            <Icon name={faTwitter} size="lg" />
+          </div>
+          <h3>Bio:</h3>
+          <p className={styles.bioText}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+            Velit officia consequat duis enim velit mollit. Exercitation veniam
+            consequat sunt nostrud amet.
+          </p>
+          <div className={styles.birthday}>
+            <h3>Birthday date:</h3>
+            <DateInput />
+          </div>
+          <div className={styles.emailBlock}>
+            <h3>Email:</h3>
+            <p>tom@cruise.com</p>
+          </div>
+          <div className={styles.wishesCount}>
+            <h3>Wishes:</h3>
+            <p>10</p>
+          </div>
         </div>
       </section>
     </div>
