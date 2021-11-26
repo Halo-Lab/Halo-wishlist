6904dabb367115efb4f49a23503f50c52c52a515
GephestUA <gephestua@gmail.com>
2021-11-26 12:42:00 +0200

    change view

--- Client/src/scenes/AdminPage/AdminPage.tsx
+++ Client/src/scenes/AdminPage/AdminPage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import { MainLayout } from '../../components/layout/MainLayout';
 import { IUser } from '../../models/IUser';
