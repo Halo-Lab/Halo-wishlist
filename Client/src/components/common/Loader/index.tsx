ce5e7142a37cce73e1944b2357d6b611d393f7ee
GephestUA <gephestua@gmail.com>
2022-01-04 12:46:04 +0200

    added loaders

--- Client/src/components/common/Loader/index.tsx
+++ Client/src/components/common/Loader/index.tsx
@@ -2,11 +2,13 @@ import styles from './Loader.module.scss';
 
 export const Loader = () => {
   return (
-    <div className={styles.ldsRing}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className={styles.loaderWrapper}>
+      <div className={styles.ldsRing}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
