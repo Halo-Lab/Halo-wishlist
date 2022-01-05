a53a798b9786276511851de3244c67712cbefb59
GephestUA <gephestua@gmail.com>
2022-01-05 10:15:26 +0200

    reloading fix

--- Chrome-Ex-BackEnd/controllers/user-controller.js
+++ Chrome-Ex-BackEnd/controllers/user-controller.js
@@ -62,8 +62,10 @@ class UserController {
   async refresh(req, res, next) {
     try {
       const { refreshToken } = req.cookies;
+      const { remember } = await TokenModal.findOne({ refreshToken });
       const userData = await userService.refresh(refreshToken);
-      await setCookie(res, userData, await TokenModal.findOne({ refreshToken }));
+      console.log(remember);
+      setCookie(res, userData, remember);
       return res.json(userData);
     } catch (e) {
       next(e);
--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,6 +4,8 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import LoginForm from './components/LoginForm/LoginForm';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import Image from './components/common/ImageComponent/Image';
+import { Loader } from './components/common/Loader';
 import { NotFound } from './scenes/404';
 import { AdminPage } from './scenes/AdminPage/AdminPage';
 import { ListPage } from './scenes/ListPage';
@@ -12,25 +14,38 @@ import { SharePage } from './scenes/SharePage';
 import { AppRootStateType } from './store/store';
 import { checkUserLogin, UserStateType } from './store/user-reducer';
 
+import logo from './assets/svg/wishyou-logo.svg';
+
 const App: FC = () => {
   const dispatch = useDispatch();
   const user = useSelector<AppRootStateType, UserStateType>((state) => state.users);
 
   useEffect(() => {
-    if (localStorage.getItem('token')) {
-      dispatch(checkUserLogin());
-    }
+    const handler = setTimeout(() => {
+      if (localStorage.getItem('token')) {
+        dispatch(checkUserLogin());
+      }
+    }, 300);
+
+    return () => {
+      clearTimeout(handler);
+    };
   }, []);
 
   if (user.isLoading) {
-    return <div></div>;
+    return (
+      <div className="main-wrapper">
+        <Image alt="wishyou logo" src={logo} width={150} height={37} />
+        <Loader />
+      </div>
+    );
   }
 
   if (!user.isLoggedIn) {
     return (
       <div className="main-wrapper">
         <Switch>
-          <Route path="/" render={() => <LoginForm />} exact />
+          <Route path="/" exact render={() => <LoginForm />} />
           <Route path="/registration" render={() => <RegistrationForm />} />
           <Route
             path={`/shared/:userNickname/:listID`}
