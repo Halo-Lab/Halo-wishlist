d0f8eb3c0392e31f43f498493be7c48e0358b205
GephestUA <gephestua@gmail.com>
2022-01-05 13:05:04 +0200

    forgote modal

---
+++ Client/src/components/ForgotPassword/index.tsx
@@ -0,0 +1,53 @@
+import { Form, Formik } from 'formik';
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
+
+import '../../utils/i18next';
+import { ButtonService } from '../common/ButtonSendForm/ButtonSendForm';
+import { FormikTextInput } from '../common/FormikInput/FormikInput';
+
+import styles from './ForgotPassword.module.scss';
+
+export const ForgotPassword: FC = () => {
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email(t('errors.notValidEmail'))
+      .max(50, t('errors.emailMaxLength'))
+      .required(t('errors.required')),
+  });
+
+  const handleSubmitForm = () => {
+    alert(1);
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        email: '',
+      }}
+      onSubmit={handleSubmitForm}
+      validationSchema={LoginSchema}
+    >
+      {({ errors }) => (
+        <Form>
+          <FormikTextInput
+            className={styles.input}
+            type="email"
+            name="email"
+            placeholder="user@gmail.com"
+          />
+
+          <ButtonService
+            btnName={t('auth.sendPassword')}
+            disabled={errors.email ? true : false}
+          />
+        </Form>
+      )}
+    </Formik>
+  );
+};
